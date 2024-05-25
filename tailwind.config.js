3ed372d54335c9e51335961560e636dcfbbcb8b5
NTThanh2711 <thanhbct5abtx@gmail.com>
2024-05-25 18:42:18 +0700

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      color:{
+        'light-coffe':'#C89F94'
+      }
+    },
   },
   plugins: [],
 }
